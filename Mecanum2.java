2b4bbdf449ae6417a06780fae83b8910abc46a35
Mateo Jimenez <tidy-scorer-stem@duck.com>
2025-12-04 17:18:17 -0800

    Clean up import statements in Mecanum2.java
    Removed duplicate import statements for LinearOpMode and DcMotor.

--- Mecanum2.java
+++ Mecanum2.java
@@ -5,12 +5,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
